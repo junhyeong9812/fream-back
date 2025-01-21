2a8d9189eff883543392ccb4463b89d6546cff4b
jun <pickjog@gmail.com>
2025-01-21 17:19:36 +0900

    rebuild Fream

---
+++ src/main/java/com/fream/fream_back/FreamBackApplication.java
@@ -0,0 +1,13 @@
+package com.fream.fream_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreamBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreamBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fream/fream_back/FreamBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fream.fream_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreamBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
