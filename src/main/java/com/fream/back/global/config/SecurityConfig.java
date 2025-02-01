2dba2c5b2611417973c67730bab202f66682bf24
jun <pickjog@gmail.com>
2025-02-01 22:39:19 +0900

    fixed security

--- src/main/java/com/fream/back/global/config/SecurityConfig.java
+++ src/main/java/com/fream/back/global/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.fream.back.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -11,6 +12,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
  * - 모든 요청 허용
  * - CORS 설정
  */
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
