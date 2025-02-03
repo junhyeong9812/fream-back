85ed091fede6244b3db683d79ac1cef23a4a7cc8
jun <pickjog@gmail.com>
2025-02-03 15:45:30 +0900

    swap import page able:

--- src/main/java/com/fream/back/domain/faq/repository/FAQRepository.java
+++ src/main/java/com/fream/back/domain/faq/repository/FAQRepository.java
@@ -4,11 +4,11 @@ package com.fream.back.domain.faq.repository;
 import com.fream.back.domain.faq.entity.FAQ;
 import com.fream.back.domain.faq.entity.FAQCategory;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.awt.print.Pageable;
 import java.util.Optional;
 
 public interface FAQRepository extends JpaRepository<FAQ, Long>, FAQRepositoryCustom  {
