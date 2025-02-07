f0c29feca959d530cefe393488431a06681dc9ee
jun <pickjog@gmail.com>
2025-02-07 16:57:23 +0900

    git redis config

---
+++ src/main/java/com/fream/back/global/config/RedisConfig.java
@@ -0,0 +1,27 @@
+package com.fream.back.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        LettuceConnectionFactory factory = new LettuceConnectionFactory("redis", 6379);
+        // 필요시 factory.setPassword("..."); etc.
+        return factory;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        // 필요시 serializer 설정
+        return template;
+    }
+}
+
