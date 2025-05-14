6d425ae296ad6fc820e90b4f101780aa86d0cc36
jun <pickjog@gmail.com>
2025-05-14 12:20:07 +0900

    fixed Inquiry RuntimeException

--- src/main/java/com/fream/back/domain/inquiry/repository/InquiryImageRepository.java
+++ src/main/java/com/fream/back/domain/inquiry/repository/InquiryImageRepository.java
@@ -13,21 +13,21 @@ import java.util.List;
 public interface InquiryImageRepository extends JpaRepository<InquiryImage, Long> {
 
     // 특정 문의의 모든 이미지 조회
-    List<InquiryImage> findAllByInquiryId(Long inquiryId);
+    List<InquiryImage> findAllByInquiry_Id(Long inquiryId);
 
     // 특정 문의의 질문 이미지만 조회
-    List<InquiryImage> findAllByInquiryIdAndIsAnswerFalse(Long inquiryId);
+    List<InquiryImage> findAllByInquiry_IdAndIsAnswerFalse(Long inquiryId);
 
     // 특정 문의의 답변 이미지만 조회
-    List<InquiryImage> findAllByInquiryIdAndIsAnswerTrue(Long inquiryId);
+    List<InquiryImage> findAllByInquiry_IdAndIsAnswerTrue(Long inquiryId);
 
     // 특정 문의의 모든 이미지 삭제
-    void deleteAllByInquiryId(Long inquiryId);
+    void deleteAllByInquiry_Id(Long inquiryId);
 
     // 특정 문의의 이미지 개수 조회
-    long countByInquiryId(Long inquiryId);
+    long countByInquiry_Id(Long inquiryId);
 
     // URL로 이미지 조회
-    @Query("SELECT i FROM InquiryImage i WHERE i.inquiryId = :inquiryId AND i.imageUrl LIKE %:imageUrl%")
-    List<InquiryImage> findByInquiryIdAndImageUrlContaining(@Param("inquiryId") Long inquiryId, @Param("imageUrl") String imageUrl);
+    @Query("SELECT i FROM InquiryImage i WHERE i.inquiry.id = :inquiryId AND i.imageUrl LIKE %:imageUrl%")
+    List<InquiryImage> findByInquiry_IdAndImageUrlContaining(@Param("inquiryId") Long inquiryId, @Param("imageUrl") String imageUrl);
 }
